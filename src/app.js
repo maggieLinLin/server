250ddec6147273b1bfac32b0959414d40418f758
maggieLinLin <155457597+maggieLinLin@users.noreply.github.com>
2025-11-29 05:22:24 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -46,7 +46,7 @@ const config = require('./cli.js')
 	.parse(process.argv);
 
 global.address = config.address;
-config.port = (config.port || '8080:8081')
+config.port = (process.env.PORT || config.port || '8080:8081')
 	.split(':')
 	.map((string) => parseInt(string));
 const invalid = (value) => isNaN(value) || value < 1 || value > 65535;
